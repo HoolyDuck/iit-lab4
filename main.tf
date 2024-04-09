3243df4f5acdb7d52390a09a9d91bf9b384de4bf
wilhelm wesser <wilhelmwesser228@gmail.com>
2024-04-09 16:12:07 +0300

    pdate

--- main.tf
+++ main.tf
@@ -70,18 +70,8 @@ resource "aws_security_group" "http_server" {
 resource "aws_vpc_security_group_ingress_rule" "http_server_http_rule" {
   security_group_id = aws_security_group.http_server.id
   cidr_ipv4 = "0.0.0.0/0"
-  # from_port = 80
-  # to_port = 80
   ip_protocol = "-1"
 }
-
-# resource "aws_vpc_security_group_ingress_rule" "http_server_ssh_rule" {
-#     security_group_id = aws_security_group.http_server.id
-#   cidr_ipv4 = "0.0.0.0/0"
-#   from_port = 22
-#   to_port = 22
-#   ip_protocol = "tcp"
-# }
 
 resource "aws_vpc_security_group_egress_rule" "http_server_outer_rule" {
   security_group_id = aws_security_group.http_server.id
